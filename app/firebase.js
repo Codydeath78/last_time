42bf03a2b918c88febc49814dda15793f90def3d
Codydeath78 <GOGETALIFEREALLYDUDE@gmail.com>
2024-07-31 18:41:50 -0700

    plz work

--- app/firebase.js
+++ app/firebase.js
@@ -8,8 +8,7 @@ import { TextField } from '@mui/material';
 import { SearchBar } from 'material-ui-search-bar';
 
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
